304960b9d2563ed14fec52fe68d3e924fb75f92a
Carlos <carloma@campusaula.com>
2017-10-24 12:03:08 +0200

    v1.1

---
+++ Calculadora/src/CalcMain.java
@@ -0,0 +1,22 @@
+
+public class CalcMain {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		new CalcGUI();
+		
+		new ValoresGuardados("SUMA", "0");
+		
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Calculadora/src/ValoresGuardados.java
@@ -0,0 +1,26 @@
+
+public class ValoresGuardados {
+
+	static String operacion = "";
+	static String numeroanterior = "0";
+	static boolean borrar = false;
+	
+	
+	
+	
+	public ValoresGuardados(String operacion, String numeroanterior) {
+		
+		ValoresGuardados.operacion = operacion;
+		ValoresGuardados.numeroanterior = numeroanterior;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
